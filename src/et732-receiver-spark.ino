c3ea40ea699201473d5bd9f62d343d57d43de5e1
bob-blake <robert.j.blake@gmail.com>
2015-04-23 22:08:52 -0400

    Get wireless data reception working.

--- src/et732-receiver-spark.ino
+++ src/et732-receiver-spark.ino
@@ -1,11 +1,12 @@
 #include "HttpClient.h"
 
 #define LOGGING   // Enable debug data from serial port
+#define LOGGING_FINE
 
 // Define pins
 int pin_led     = D1;
 int pin_data_in = A1;
-int pin_debug   = A2;
+int pin_debug   = D6;
 
 // Declare Global HTTP variables
 HttpClient http;
@@ -23,6 +24,8 @@ http_header_t headers[] = {
 #define OOK_PRE_HIGH_MIN  150   // us
 #define OOK_PRE_HIGH_MAX  250   // us
 
+#define RX_PRE_TIMEOUT  40000   // us
+
 #define NUM_PRE_PULSES  8
 #define NUM_RX_BYTES    13
 #define NUM_RX_NIBBLES  NUM_RX_BYTES * 2
@@ -39,8 +42,7 @@ http_header_t headers[] = {
 
 enum rx_states{
   RX_STATE_IDLE,
-  RX_STATE_CHECK_PRE,
-  RX_STATE_SYNC,
+  RX_STATE_PREAMBLE,
   RX_STATE_RECEIVE
 };
 volatile enum rx_states state=RX_STATE_IDLE;
@@ -58,7 +60,7 @@ void setup() {
 
     #ifdef LOGGING
       // Configure USB serial port
-      Serial.begin(9600);
+      Serial.begin(115200);
       // On Windows it is necessary to implement the following line:
       // Make sure your Serial Terminal app is closed before powering your Core
       // Now open your Serial Terminal, and hit any key to continue!
@@ -68,38 +70,41 @@ void setup() {
 
     // Configure pin I/O
     pinMode(pin_led, OUTPUT);
-    pinMode(pin_data_in, INPUT);
+    pinMode(pin_data_in, INPUT_PULLUP);
     pinMode(pin_debug, OUTPUT);
-
+    digitalWrite(pin_debug,LOW);
     // Set up interrupt for incoming data
     attachInterrupt(pin_data_in,interrupt_ext,CHANGE);
 }
 
 void loop() {
-  if(rx_done){
-    Serial.println("Data!");
-    rx_done = 0;
-  }
-  /*
-  if(state > RX_STATE_IDLE){
-    Serial.print("State: ");
-    Serial.println(state);
-  }
-  if(rx_err > 1){
-    Serial.print("Err: ");
-    Serial.println(rx_err);
-  }*/
+  unsigned int i;
+  #ifdef LOGGING
+    if(rx_done){
+      Serial.print("Data: ");
+      for(i=0;i<sizeof(rx_data);i++){
+        Serial.print(rx_data[i],DEC);
+      }
+      Serial.print("\r\n");
+      rx_done = 0;
+    }
+
+    //if(rx_err > 1){
+//      Serial.print("Err: ");
+      //Serial.println(rx_err);
+    //}
+  #endif
 }
 
 // Use state machine here
 // Idle, check preamble, receive data
 void interrupt_ext() {
-  static int pre_counter=0, bit_counter=0;
-  static unsigned long start_time;
-  unsigned long now_time, pulse_width;
-  bool  edge, waiting;
+  volatile static int pre_counter=0, bit_counter=0, waiting=0;
+  volatile static unsigned long start_time=0;
+  volatile unsigned long now_time, pulse_width, timeout_counter, timeout;
+  volatile bool  edge;
 
-  digitalWrite(pin_debug,HIGH);
+
   now_time = micros(); // Save time  and edge immediately for best accuracy
   edge = digitalRead(pin_data_in);
   pulse_width = now_time - start_time;
@@ -107,51 +112,58 @@ void interrupt_ext() {
   switch(state){
 
     case RX_STATE_IDLE:
-      if(edge == LOW){
-        pre_counter = 0;
+      if(edge == HIGH){ // Received a low pulse
+        timeout_counter = 0;
         bit_counter = 0;
         waiting = 0;
         rx_err = 0;
-        //rx_done = 0;
-        state = RX_STATE_CHECK_PRE;
-      }
-    break;
-
-    case RX_STATE_CHECK_PRE:
-      if(edge == HIGH){ // Low-high transition
         if((pulse_width >= OOK_PRE_LOW_MIN) && (pulse_width <= OOK_PRE_LOW_MAX)){
-          pre_counter++;
-          if(pre_counter == NUM_PRE_PULSES){ // We have received the entire preamble
-            state = RX_STATE_SYNC;
-          }
+            timeout_counter = now_time; // Mark time from the end of first low pulse
+            state = RX_STATE_PREAMBLE;
         }
         else{ // Low pulse was too short
           rx_err = 1;
-          state = RX_STATE_IDLE;
-        }
-      }
-      else{ // High-low transition
-        if((pulse_width >= OOK_PRE_HIGH_MIN) && (pulse_width <= OOK_PRE_HIGH_MAX)){
-          // Do nothing
-        }
-        else{ // High pulse was too short
-          rx_err = 2;
-          state = RX_STATE_IDLE;
         }
       }
     break;
 
-    case RX_STATE_SYNC:
-      if((pulse_width >= TWO_T_MIN) && (pulse_width <= TWO_T_MAX)){  // Wait for 2T to start counting
-        rx_data[bit_counter] = edge;
-        bit_counter++;
-        state = RX_STATE_RECEIVE;
+    case RX_STATE_PREAMBLE:
+      if(edge == LOW){ // High-low transition
+        if((pulse_width >= TWO_T_MIN) && (pulse_width <= TWO_T_MAX)){
+          rx_data[bit_counter] = 1; // There must have been a rising edge first
+          bit_counter++;
+          rx_data[bit_counter] = 0;  // That's the falling edge we just got
+          digitalWrite(pin_debug,HIGH);
+          bit_counter++;
+          state = RX_STATE_RECEIVE;
+        }
+        else if((pulse_width < T_MIN) || (pulse_width > TWO_T_MAX)){
+          rx_err = 2;   // High pulse was too short or too long
+          state = RX_STATE_IDLE;
+        }
+      }
+      else if(edge == HIGH){ // Low-high transition
+        if((pulse_width < OOK_PRE_LOW_MIN) || (pulse_width > OOK_PRE_LOW_MAX)){
+          rx_err = 3;   // Low pulse was too long or short
+          state = RX_STATE_IDLE;
+        }
       }
     break;
 
     case RX_STATE_RECEIVE:
+      if(edge == HIGH){
+        if(pulse_width >= OOK_PRE_LOW_MIN){
+          rx_err = 3;
+          rx_done = 1;            // If we receive a long low pulse, call it done
+          Serial.print("Err: ");
+          Serial.println(rx_err);
+          state = RX_STATE_IDLE;
+        }
+      }
+
       if((pulse_width >= T_MIN) && (pulse_width <= T_MAX)){ // If T
-        if(!waiting){
+
+        if(waiting == 0){
           waiting = 1;  // Wait to capture next edge, make sure this is also T
         }
         else{
@@ -161,8 +173,10 @@ void interrupt_ext() {
         }
       }
       else if((pulse_width >= TWO_T_MIN) && (pulse_width <= TWO_T_MAX)){
-        if(waiting){  // Only one consecutive T means an error, reset
-          rx_err = 3;
+        if(waiting == 1){  // Only one consecutive T means an error, reset
+          rx_err = 4;
+          Serial.print("Err: ");
+          Serial.println(rx_err);
           state = RX_STATE_IDLE;
         }
         // 2T means the bit changes
@@ -175,13 +189,14 @@ void interrupt_ext() {
       }
       if(bit_counter >= NUM_RX_BITS){ // Full message received
         rx_done = 1;            // Maybe add something to protect data array against new data coming in quickly
+        digitalWrite(pin_debug,LOW);
         state = RX_STATE_IDLE;
       }
     break;
   }
 
   start_time = now_time;
-  digitalWrite(pin_debug,LOW);
+
 }
 
 // Sends data to Bob's BBQ Site
